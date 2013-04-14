54b27d47dc0393ba9d4c971f9908d711f0ffddfe
Andrew Hughson <andrew@hughson.me>
2013-04-14 16:31:42 +1200

    Add and load from some sample data

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import os
+import json
 import urlparse
 import psycopg2
 
@@ -11,16 +12,28 @@ cur = conn.cursor()
 
 
 def get_patients():
-    return {"hi": 1}
+    with open("sampledata/patients.json") as f:
+        return json.loads(f.read())
 
 
 def get_patient(patient_id):
-    return {"hi": 1}
+    with open("sampledata/patients.json") as f:
+        patients = json.loads(f.read())
+
+        for patient in patients.get('patients'):
+            if patient.get('patient_id') == patient_id:
+                return patient
 
 
 def get_vital_infos():
-    return {"hi": 1}
+    with open("sampledata/vitalinfos.json") as f:
+        return json.loads(f.read())
 
 
 def get_vital_info(vitalinfo_id):
-    return {"hi": 1}
+    with open("sampledata/vitalinfos.json") as f:
+        vitalinfos = json.loads(f.read())
+
+        for vitalinfo in vitalinfos.get('vitalinfo'):
+            if vitalinfo.get('vitalinfo_id') == vitalinfo_id:
+                return vitalinfo
