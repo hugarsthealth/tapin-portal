152a0d3ec05c0a082c53122c183082b43a3302aa
Andrew Hughson <andrew@hughson.me>
2013-04-14 15:31:04 +1200

    Connect to postgres

--- vsm_server.py
+++ vsm_server.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 
 import os
+import urlparse
 import psycopg2
 
 from flask import Flask
 
 
 app = Flask(__name__)
-dburl = os.environ.get("DATABASE_URL")
-conn = psycopg2.connect(dburl)
+
+urlparse.uses_netloc.append('postgres')
+url = urlparse.urlparse(os.getenv('DATABASE_URL'))
+
+conn = psycopg2.connect("dbname=%s user=%s password=%s host=%s " % (url.path[1:], url.username, url.password, url.hostname))
+cur = conn.cursor()
 
 
 @app.route('/')
 def hello():
-    return dburl + conn
+    return url
 
 
 @app.route('/patients')
@@ -21,17 +26,17 @@ def patients():
     pass
 
 
-@app.route('/patients/<int:id>', methods=['GET', 'POST'])
-def patient(id):
+@app.route('/patients/<int:patient_id>', methods=['GET', 'POST'])
+def patient(patient_id):
     pass
 
 
-@app.route('/patients/<int:id>/vitalinfo')
+@app.route('/patients/<int:patient_id>/vitalinfo')
 def vital_info():
     pass
 
 
-@app.route('/patients/<int:id>/vitalinfo/<int:id>', methods=['GET', 'POST'])
+@app.route('/patients/<int:patient_id>/vitalinfo/<int:vitalinfo_id>', methods=['GET', 'POST'])
 def vital_info_from_id(id):
     pass
 
