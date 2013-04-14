4464e1e5b82b11c6401275d743f6986a4f5e5212
Andrew Luey <lueylueyluey@gmail.com>
2013-04-14 16:38:57 +1200

    NHI format fixed

--- json_gen.py
+++ json_gen.py
@@ -7,20 +7,31 @@ import string
 def rand_lower_str(length):
     return ''.join(choice(string.lowercase) for i in range(length))
 
-def rand_nhi(length):
-    return ''.join(choice(string.uppercase + string.digits) for i in range(length))
+def rand_nhi():
+    letters = ''.join(choice(string.uppercase) for i in range(3))
+    digits =  ''.join(choice(string.digits) for i in range(3))
+    return letters + digits
 
 def rand_bool():
     return choice(['Yes', 'No'])
 
+def write_to_file(filename, data):
+    f = open(filename, 'w')
+    f.writelines(data)
+    f.close
+
+patient_id = randrange(0, 100000)
 firstname = rand_lower_str(10).capitalize()
 lastname = rand_lower_str(10).capitalize()
-nhi = rand_nhi(6)
+nhi = rand_nhi()
 occupation = rand_lower_str(20).capitalize()
 citizen_resident = 'Yes'
 contact_num = randrange(100000, 999999)
 gender = choice(['Male', 'Female'])
 dob = rand_lower_str(20)
+
+vital_info_id = randrange(0, 10000000)
+vital_info_url = "/patients/%s/vitalinfo" % patient_id
 weight_value = rand_lower_str(20)
 weight_unit = choice(['kg', 'lb'])
 height_value = rand_lower_str(20)
@@ -35,6 +46,7 @@ medical_conditions = rand_lower_str(20)
 allergies = rand_lower_str(20)
 
 patient = {
+    'patient_id': patient_id,
     'firstname': firstname,
     'lastname': lastname,
     'nhi': nhi,
@@ -43,6 +55,11 @@ patient = {
     'contact_num': contact_num,
     'gender': gender,
     'dob': dob,
+}
+
+vital_info = {
+    'vital_info_id': vital_info_id,
+    'vital_info_url': vital_info_url,
     'weight': {'value': weight_value, 'unit': weight_unit},
     'height': {'value': height_value, 'unit': height_unit},
     'blood_type': blood_type,
@@ -54,5 +71,8 @@ patient = {
     'allergies': allergies
     }
 
-j = json.dumps(patient, sort_keys=True, indent=4)
-print(j)
+patient_data = json.dumps(patient, sort_keys=True, indent=4)
+vital_info_data = json.dumps(vital_info, sort_keys=True, indent=4)
+
+write_to_file('patient.json', patient_data)
+write_to_file('vital_info.json', vital_info_data)
