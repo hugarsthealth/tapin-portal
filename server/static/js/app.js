24240d90da90b3dcc2f1c9112df0ce36685e20fc
Dave Carpenter <dave@ungarst.com>
2013-04-29 14:17:09 +1200

    Improve searching to pick if searching by name or nhi

--- server/static/js/app.js
+++ server/static/js/app.js
@@ -6,11 +6,27 @@ var myApp = angular.module('vsmApp', [], function($interpolateProvider) {
 function PatientListCtrl($scope, $http){
   $http.get('/patients/').success(function(data) {
     $scope.patients = data.patients;
+    console.log($scope.patients);
     for (var i = 0 ; i < $scope.patients.length ; i++) {
       $scope.patients[i].fullname = $scope.patients[i].firstname + $scope.patients[i].lastname;
     }
   });
 
   $scope.orderProp = 'patient_id';
-}
+  $scope.searchBy = "fullname";
 
+  $scope.searchBarChange = function() {
+    $scope.query = {};
+    if ($scope.searchBy === "fullname") {
+      $scope.query.fullname = $scope.queryString;
+    } else {
+      $scope.query.nhi = $scope.queryString;
+    }
+    console.log($scope.query);
+  };
+
+  $scope.searchByChange = function () {
+    $scope.searchBarChange();
+  };
+
+}
