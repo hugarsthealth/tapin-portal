3be1e7d4c6d0dae1c579d3204a64a15de3c83fca
Andrew Hughson <andrew@hughson.me>
2013-05-08 01:08:08 +1200

    Fix sessions not closing on request ending

--- server/__init__.py
+++ server/__init__.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 import os
+
 from flask import Flask
+
+from models import db
 
 app = Flask(__name__)
 app.config['DEBUG'] = False if "PRODUCTION" in os.environ else True
+
+
+@app.teardown_request
+def shutdown_session(exception=None):
+    db.remove()
 
 import server.vsm
